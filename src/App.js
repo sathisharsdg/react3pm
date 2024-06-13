3e713a6ec6704e061026aab33bb9fe9acbc5b58a
sathisharsdg <sathish.333ddd@gmail.com>
2024-06-13 15:39:33 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function App() {
-  
+    const opentext = ()=>{
+        console.log("Hi im clicked");
+    }
   return (
      <>
         <h1>Welcome Aimore</h1>
+         <button onClick={opentext}>CLICK</button>
      </>
   )
 }
