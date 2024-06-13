ca8b590a751df6e85979a9bd3f86e2945b8b6b3a
sathisharsdg <sathish.333ddd@gmail.com>
2024-06-13 16:05:20 +0530

    latest changes

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function Aboutus() {
+function Aboutus({address,arrdata}) {
+  console.log(arrdata);
   return (
     <div>
-  <h1>wwwe</h1>
-  <h1>wwwe</h1>
+        <h1>Welocome to About</h1>
+        <h1>{address}</h1>
     </div>
    
   )
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Gallery() {
+function Gallery({newaddress}) {
   return (
     <div>
        <h2>Welcome to Gallery component</h2>
+       <h1>{newaddress}</h1>
     </div>
   )
 }
