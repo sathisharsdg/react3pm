152c1f1ffb331e01af9c23ab92fdbef6694b6fdc
sathisharsdg <sathish.333ddd@gmail.com>
2024-06-28 17:05:02 +0530

    june28

--- src/API_DATA/Api_URL.jsx
+++ src/API_DATA/Api_URL.jsx
@@ -1,2 +1,4 @@
-export const API_URL = "https://jsonplaceholder.typicode.com";
+export const API_URL = "https://665969a6de346625136c4408.mockapi.io";
+export const NEWS_API = "https://newsapi.org/v2/top-headlines";
+// country=us&apiKey=da083256778d4c069bc37db6aeebff4f
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './Pages/Home'
-import Sampledata from './Pages/Sampledata'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { NEWS_API } from './API_DATA/Api_URL';
 function App() {
+  const [newsData,setnewsData] = useState([]);
+  const [country,setcountry] = useState("");
+  console.log(country);
+  const newsApi = ()=>{
+      axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=da083256778d4c069bc37db6aeebff4f`)
+      .then((res)=>{
+          setnewsData(res.data.articles);           
+      })
+      .catch((err)=>{
+          console.log(err , "Error came");
+      })
+  }
+const getCountry = ()=>{
+  axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=da083256778d4c069bc37db6aeebff4f`)
+  .then((res)=>{
+      setnewsData(res.data.articles);           
+  })
+  .catch((err)=>{
+      console.log(err , "Error came");
+  })
+}
+  useEffect(()=>{
+    newsApi()
+  },[])
   return (
     <div>
-      <BrowserRouter>
-        <ul class="nav">
-          <li class="nav-item">
-            <Link to="/" className='nav-link'>HOME</Link>         
-             
-          </li>
-          <li class="nav-item">
-          <Link to="/sample" className='nav-link'>SAMPLE DATA</Link>
-          </li>
-         
-        </ul>
-        <Routes>
-          <Route path='/' exact element={<Home />}></Route>
-          <Route path='/sample' exact element={<Sampledata />}></Route>
-        </Routes>
-      </BrowserRouter>
+         <div className="container">
+          <div className="row">
+              <div className="col-md-10">
+                  <select className='form-control' onChange={e=>setcountry(e.target.value)}>
+                     <option value="in">INDIA</option>
+                     <option value="us">USA</option>
+                     <option value="jp">JAPAN</option>
+                     <option value="ca">CANADA</option>
+                     <option value="nz">NEWZLAND</option>
+                     <option value="bg">BELGIUM</option>
+                     <option value="si">SIII</option>
+                     <option value="fr">FRANCE</option>
+                  </select>
+                  
+              </div>
+              <div className="col-md-2">
+                  <button className='btn btn-success' onClick={getCountry}>GET DETAILS</button>
+              </div>
+          </div>
+            <div className="row mt-3 mb-3">
+                {
+                    newsData.map((res)=>{
+                         return(
+                          <div className='col-md-4'>
+                          <h6>{res.title}</h6>
+                          <img src={res.urlToImage} className='img-fluid'/>
+                          </div>
+                         )
+                    })
+                }
+            </div>
+         </div>
     </div>
   )
 }
